e11cab2387eb8374e5340df290dcb807ca7d155d
jiss00 <ksksksk729@gmail.com>
2024-04-29 15:57:16 +0900

    2024-04-29 수정,커밋 api 연동

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -29,10 +29,10 @@ function Readme() {
   const addTeamInfoToMarkdown = (day, introduce, teamMembers) => {
     // 기존 마크다운 분할을 위한 정규식
     const regex = /\n##/;
-  
+
     // 기존 마크다운을 분할하여 배열에 저장
     const parts = markdown.split(regex);
-  
+
     // 새로운 내용 구성
     let newContent = `\n## 📅개발기간\n${day}\n\n`;
     newContent += `## 👥팀원소개\n### ${introduce}\n\n`;
@@ -41,40 +41,41 @@ function Readme() {
       newContent += `   - **학교:** 🏫${member.school}\n`;
       newContent += `   - **역할:** 🌐${member.role}\n`;
     });
-  
+
     // 새로운 내용과 기존 마크다운의 나머지 부분을 합쳐서 새로운 마크다운 생성
     const newMarkdown = parts[0] + newContent + '\n##' + parts.slice(1).join('\n##');
-  
+
     setMarkdown(newMarkdown);
   };
   const commit = async () => {
     const userToken = localStorage.getItem('refreshToken');
 
     try {
-        const requestBody = {
-            //토큰 있어야함.
-            name: repositoryName,
-            content: markdown
-        };
-        const response = await axios.post('http://3.39.11.243:8080/api/readme/commit', requestBody, {
-          headers: {
-            Authorization: `Bearer ${userToken}`
-          }
-        });
-
-        //const response = await axios.post('http://3.39.11.243:8080/api/readme/commit', requestBody);
-        console.log(response.data); // 서버로부터 받은 응답 데이터 처리
-
-        return response.data; // 선택적으로 응답 데이터 반환
-    } catch (error) {
-        console.error('Error while posting commit:', error);
-        throw error; // 예외 처리
-    }
-};
-const save = async () => {
-  const userToken = localStorage.getItem('refreshToken');
-  try {
       const requestBody = {
+        //토큰 있어야함.
+        repositoryName: repositoryName,
+        content: markdown
+      };
+      const response = await axios.post('http://3.39.11.243:8080/api/readme/commit', requestBody, {
+        headers: {
+          Authorization: `Bearer ${userToken}`
+        }
+      });
+
+      console.log(response); // 서버로부터 받은 응답 데이터 처리
+      alert("커밋완료");
+
+      return response.data; // 선택적으로 응답 데이터 반환
+    } catch (error) {
+      console.error('Error while posting commit:', error);
+      throw error; // 예외 처리
+    }
+  };
+  const save = async () => {
+    const userToken = localStorage.getItem('refreshToken');
+    try {
+      const requestBody = {
+        //토큰 있어야함.
         name: repositoryName,
         content: markdown
       };
@@ -84,14 +85,14 @@ const save = async () => {
         }
       });
 
-      console.log(response.data); // 서버로부터 받은 응답 데이터 처리
-
+      console.log(response); // 서버로부터 받은 응답 데이터 처리
+      alert("저장 완료");
       return response.data; // 선택적으로 응답 데이터 반환
-  } catch (error) {
+    } catch (error) {
       console.error('Error while posting commit:', error);
       throw error; // 예외 처리
-  }
-};
+    }
+  };
 
   return (
     <>
@@ -106,7 +107,7 @@ const save = async () => {
       <div className='grid_div'>
         <div className='add_box' onClick={add}>Add</div>
         <div className='save_box' onClick={save}>Save</div>
-        <div className='commit_box'onClick={commit}>Commit to repository</div>
+        <div className='commit_box' onClick={commit}>Commit to repository</div>
       </div>
       <Modal closeModal={closeModal} isOpen={isModalOpen} addTeamInfoToMarkdown={addTeamInfoToMarkdown}></Modal>
     </>
